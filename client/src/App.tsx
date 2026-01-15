bd257b34fa4624f5f1fb68c6cc922c3f30719f8d
PushkarChandra <pushkarchandra2005@gmail.com>
2026-01-15 16:03:39 +0530

    conflict resolved in app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,28 +2,14 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
+import { AuthProvider } from "@/hooks/useAuth";
+
 import NotFound from "@/pages/not-found";
 import HomePage from "@/pages/HomePage";
 import WaitlistPage from "@/pages/WaitlistPage";
 import AuthPage from "@/pages/AuthPage";
 import DashboardPage from "@/pages/DashboardPage";
-<<<<<<< HEAD
-import { AuthProvider } from "@/hooks/useAuth";
-
-function Router() {
-    return (
-        <Switch>
-            <Route path="/" component={HomePage} />
-            <Route path="/auth" component={AuthPage} />
-            <Route path="/waitlist" component={WaitlistPage} />
-            <Route path="/dashboard" component={DashboardPage} />
-            <Route component={NotFound} />
-        </Switch>
-    );
-=======
-import WaitlistPage from "@/pages/WaitlistPage";
-import NotFound from "@/pages/not-found";
-import HomePage from "@/pages/HomePage"
+import LandingPage from "@/pages/LandingPage";
 
 function Router() {
   return (
@@ -36,18 +22,17 @@ function Router() {
       <Route component={NotFound} />
     </Switch>
   );
->>>>>>> 5f3de75ab183b82d4bf537ba80012103526b0b2d
 }
 
 function App() {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <AuthProvider>
-                <Router />
-                <Toaster />
-            </AuthProvider>
-        </QueryClientProvider>
-    );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Router />
+        <Toaster />
+      </AuthProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
